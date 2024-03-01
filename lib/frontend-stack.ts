75d0743aab9a8f9a036a8dacfd1ed5117c8973d0
Michele Ricciardi <mriccia@amazon.com>
2024-03-01 18:50:08 +0100

    working deployment

--- lib/frontend-stack.ts
+++ lib/frontend-stack.ts
@@ -129,7 +129,7 @@ export class FrontendStack extends NestedStack {
                   childProcess.execSync('cd ui/node_modules || (cd ui && npm ci)', execOptions);
                   childProcess.execSync('cd ui && npm run build', execOptions);
 
-                  fsExtra.copySync('ui/build', outputDir);
+                  fsExtra.copySync('ui/out', outputDir);
                   return true;
                 } catch {
                   return false;
