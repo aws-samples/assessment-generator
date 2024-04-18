a18a239803393c8236d1bed843e236234831a14a
Michele Ricciardi <mriccia@amazon.com>
2024-04-18 16:00:34 +0200

    add ability to select role at signup

--- lib/lambdas/postConfirmation.ts
+++ lib/lambdas/postConfirmation.ts
@@ -4,20 +4,41 @@ import { DynamoDBDocumentClient, PutCommand } from "@aws-sdk/lib-dynamodb";
 import { LambdaInterface } from "@aws-lambda-powertools/commons/lib/esm/types";
 import { logger, tracer } from "../rag-pipeline/lambdas/event-handler/utils/pt";
 import { getParameter } from '@aws-lambda-powertools/parameters/ssm';
+import { AdminAddUserToGroupCommand, CognitoIdentityProviderClient } from "@aws-sdk/client-cognito-identity-provider";
 
 
 const client = new DynamoDBClient();
 const documentClient = DynamoDBDocumentClient.from(client);
 
+
+const cognitoIdentityProviderClient = new CognitoIdentityProviderClient();
+
 class Lambda implements LambdaInterface {
+  private userPoolId: string;
+
   @tracer.captureLambdaHandler()
   @logger.injectLambdaContext({ logEvent: true })
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  async handler(event: PostConfirmationTriggerEvent, lambdaContext: Context): Promise<PostConfirmationTriggerEvent> {
+  async handler(event: PostConfirmationTriggerEvent, lambdaContext: Context) {
+    this.userPoolId = event.userPoolId;
+    const { userAttributes } = event.request;
+    const userRole = userAttributes['custom:role'];
+    switch (userRole) {
+      case "teachers":
+        await this.registerTeacher(userAttributes);
+        break;
+      case "students":
+        await this.registerStudent(userAttributes);
+        break;
+      default:
+        throw new Error("Invalid role selected");
+    }
+  }
 
+  async registerStudent(userAttributes) {
     const STUDENTS_TABLE = await getParameter(process.env.STUDENTS_TABLE_PARAM);
     const createdAt = new Date().toJSON();
-    const { userAttributes } = event.request;
+    await this.assignToGroup(userAttributes["sub"], "students");
 
     const data = {
       id: userAttributes['sub'],
@@ -31,8 +52,21 @@ class Lambda implements LambdaInterface {
     });
 
     await documentClient.send(command);
+  }
 
-    return event;
+  async registerTeacher(userAttributes) {
+    await this.assignToGroup(userAttributes["sub"], "teachers");
+  }
+
+  async assignToGroup(sub, group) {
+    let request = new AdminAddUserToGroupCommand({
+      UserPoolId: this.userPoolId,
+      Username: sub,
+      GroupName: group,
+    });
+    const response = await cognitoIdentityProviderClient.send<AdminAddUserToGroupCommand>(request);
+    logger.info(response as any);
+    return response;
   }
 }
 
